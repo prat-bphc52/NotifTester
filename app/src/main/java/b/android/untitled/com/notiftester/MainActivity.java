3f6d05d6aaaaa85a3cad46a038ff28247e8e3585
prat-bphc52 <hp.agarwal1@gmail.com>
2018-09-09 15:02:08 +0530

    Fix Notification Issue

--- app/src/main/java/b/android/untitled/com/notiftester/MainActivity.java
+++ app/src/main/java/b/android/untitled/com/notiftester/MainActivity.java
@@ -1,6 +1,12 @@
 package b.android.untitled.com.notiftester;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
@@ -37,18 +43,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFunctions = FirebaseFunctions.getInstance();
-        FirebaseMessaging.getInstance().subscribeToTopic("pushNotifications")
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        String msg = getString(R.string.msg_subscribed);
-                        if (!task.isSuccessful()) {
-                            msg = getString(R.string.msg_subscribe_failed);
+
+        final SharedPreferences preferences = getSharedPreferences("MyPrefs",MODE_PRIVATE);
+        boolean flag = preferences.getBoolean("subscribed",false);
+        if(!flag) {
+            FirebaseMessaging.getInstance().subscribeToTopic("pushNotifications")
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            String msg = getString(R.string.msg_subscribed);
+
+                            if (!task.isSuccessful()) msg = getString(R.string.msg_subscribe_failed);
+                            else preferences.edit().putBoolean("subscribed",true).apply();
+
+                            Log.d("MainActivity", msg);
+                            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
                         }
-                        Log.d("MainActivity", msg);
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    }
-                });;
+                    });
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("Updates",
+                    "ATMOS Updates", NotificationManager.IMPORTANCE_HIGH);
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                    .build();
+            notificationChannel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),audioAttributes);
+            notificationChannel.setLockscreenVisibility(android.app.Notification.VISIBILITY_PUBLIC);
+            NotificationManager manager =(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(notificationChannel);
+        }
 
         final TextInputEditText mTxt = findViewById(R.id.input_text);
         Button btn = findViewById(R.id.btn);
--- app/src/main/java/b/android/untitled/com/notiftester/MyFirebaseMessagingService.java
+++ app/src/main/java/b/android/untitled/com/notiftester/MyFirebaseMessagingService.java
@@ -1,6 +1,10 @@
 package b.android.untitled.com.notiftester;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,14 +24,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d("FIREBASE MESSAGING","Callback is successful Jai Mahishmathi");
         String s = remoteMessage.getData().get("body");
         Log.d("FIREBASE MESSAGING",s);
+
+        NotificationManager manager =(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("Updates",
+                    "ATMOS Updates", NotificationManager.IMPORTANCE_HIGH);
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                    .build();
+            notificationChannel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),audioAttributes);
+            notificationChannel.setLockscreenVisibility(android.app.Notification.VISIBILITY_PUBLIC);
+            manager.createNotificationChannel(notificationChannel);
+        }
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder( this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.googleg_disabled_color_18)
                 .setContentTitle("Firebase Message Title")
                 .setContentText(s)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         Log.d("FIREBASE MESSAGING","Notification Builder is built");
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        notificationManager.notify(1, mBuilder.build());
+        manager.notify(1, mBuilder.build());
         Log.d("FIREBASE MESSAGING","Notification sent");
     }
 
